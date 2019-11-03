6a3ff822a5c697c7363b41cfdd165ea2b90a9b11
wtildestar <31348327+wtildestar@users.noreply.github.com>
2019-11-03 22:08:57 +0500

    Initial Commit

---
+++ GeoPlace/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GeoPlace
+//
+//  Created by wtildestar on 03/11/2019.
+//  Copyright © 2019 wtildestar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
