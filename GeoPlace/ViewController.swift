c0b2c9281f5072bb6e74b5c940cb055bc6e9e2d9
wtildestar <31348327+wtildestar@users.noreply.github.com>
2019-11-04 23:41:37 +0500

    set launchscreen

--- GeoPlace/ViewController.swift
+++ GeoPlace/ViewController.swift
@@ -8,13 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    let restaurantNames = [
+        "Черный ручей", "Несколько ночей", "Закатные колонны",
+        "Широкая равнина", "Луг сатиров", "Сторожевая излучина",
+        "Мертвое ущелье", "Красные пещеры", "Лодочные колонны",
+        "Звенящее логово", "Сияющее гнездо", "Стылая пещера",
+        "Bled Swang", "Turry Li", "Merge You Kneel", "Your Space Here"
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
+    // MARK: - Table view data source
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return restaurantNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        cell?.textLabel?.text = restaurantNames[indexPath.row]
+        cell?.imageView?.image = UIImage(named: restaurantNames[indexPath.row])
+        cell?.imageView?.layer.cornerRadius = cell!.frame.size.height / 2
+        cell?.imageView?.clipsToBounds = true
+        return cell!
+    }
+    
+    // MARK: - Table view delegate
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 85
+    }
 
 }
 
